92cc0c05edf19ab02140cee3af1153af4f247880
alamichaimaa <113559356+alamichaimaa@users.noreply.github.com>
2025-07-24 23:39:25 +0100

    day 1 challenge done

--- day.01/code.py
+++ day.01/code.py
@@ -1,10 +1,9 @@
-import os
+#function to count the tallest candles
 def birthdayCakeCandles(candles):
-    # Write your code here
     tallest = candles.count(max(candles))
     return tallest
 
-
+#example to test the function
 if __name__ == '__main__':
 
     candles = [3 ,2, 1, 3]
